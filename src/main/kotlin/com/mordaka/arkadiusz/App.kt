c7a2cc430520989a7c513c00012187d526d1f897
MORDAKA Arkadiusz <arkadiusz.mordaka@idemia.com>
2021-03-08 18:32:19 +0100

    Add internal modifier

--- src/main/kotlin/com/mordaka/arkadiusz/App.kt
+++ src/main/kotlin/com/mordaka/arkadiusz/App.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class App
+internal class App
 
 fun main(args: Array<String>) {
     runApplication<App>(*args)
--- src/main/kotlin/com/mordaka/arkadiusz/domain/Config.kt
+++ src/main/kotlin/com/mordaka/arkadiusz/domain/Config.kt
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @Configuration
-class SecurityConfig : WebSecurityConfigurerAdapter() {
+internal class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     override fun configure(auth: AuthenticationManagerBuilder) {
         auth.inMemoryAuthentication()
